ae98db1a6ce88a95e4698da51eb6244116cecb97
Anton Smirnov <a.y.smirnov@student.khai.edu>
2023-10-03 09:49:50 +0200

    ChatBot 5-st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -22,3 +22,24 @@ print("Now I will prove to you that I can count to any number you want")
 iterations_count = int(input())
 for i in range(iterations_count + 1):
     print(f"{i}!")
+
+print("Let`s test your programming knowledge")
+print("How to set the value to Username in Git?")
+print("1. All commands except git user.name")
+print("2. git user.name")
+print("3. git config --global user.name")
+print("4. git config user.name")
+
+answer = int(input())
+
+while True:
+    if answer < 1 or answer > 4:
+        print("There is no such answer. Try from 1 to 4")
+    else:
+        if answer == 2:
+            print("Completed, have a nice day!")
+            break
+        else:
+            print("Please, try again.")
+            answer = int(input())
+print("Congratulations, have a nice day!")
