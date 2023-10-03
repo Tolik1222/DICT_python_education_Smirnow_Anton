55903d9681d1a380a45042ca7365a43e091c36fa
Anton Smirnov <a.y.smirnov@student.khai.edu>
2023-10-03 09:26:01 +0200

    ChatBot 3-st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -4,5 +4,15 @@ print("Please, remind me your name.")
 
 your_name = input()
 
-print(f"What a great ma,e you have")
-gi
+print(f"What a great name you have")
+
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
