b4ac504d6bb0dba3cc14be9874f1c85c659680bb
Anton Smirnov <a.y.smirnov@student.khai.edu>
2023-10-17 10:27:23 +0200

    Zoo 3-st stage

--- Zoo/zoo.py
+++ Zoo/zoo.py
@@ -23,3 +23,103 @@ The camel habitat...
            ;,,      !,!   !,!     ;,;
            /_I      L_I   L_I     /_I
 Look at that!"""
+
+rabbit = r"""
+The rabbit habitat...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \
+\//\/      .- \
+ Y        /    Y    
+ l       I     !
+ ]\      _\    /"\
+(" ~----( ~   Y.  )
+It looks fine!"""
+
+deer = r"""
+The deer habitat...
+   /|       |\
+`__\\       //__'
+
+ \__`\     |'__/
+   `_\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           |  |           | |   | |
+           |  |           | |   | |
+           |  |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Pretty good!"""
+
+bat = r"""
+The bat habitat...
+ /\                 /\
+/ \'._   (\_/)   _.'/ \
+|.''._'--(o.o)--'_.''.|
+ \_ / ;=/ " \=; \ _/
+   \__| \___/ |__/
+        \(_|_)/
+         " ` "
+It's doing fine."""
+
+lion = r"""
+The lion habitat...
+                                     , w.
+                                , YWMMw, M,
+                              'MMMMMw,wMWmW,
+_. - ""        '''           YP"WMMMMMMMMMb,
+                 .-'  __.'                  .'     MMMMW^WMMMM;
+    _,         .' .-'";   `,    /`    .--""       :MMM[==MWMW^;
+ ,mM^"      ,-' .'   /     ;    ;    /     ,      MMMMb_wMW" @\
+,MM:.     .' .-'   .'      ;    `\   ;      `,    MMMMMMMW `"=./`-,
+WMMm__,-'.' .`    /       _.\     F''' - +,, ;_, _.dMMMMMMMM[, _ / ` = _}
+"^MP__.-'    ,-'   _.-""   `-,    ;    \    ; ;MMMMMMMMMMW^``; __|
+            /    .'     '-,  ;      \      )  `{  \  `
+           /   .'         /  ;     .`    /
+          /   Y,          ', (-,=,_{     ;
+          (--, )            ',_/`)  \/"")
+The lion is roaring!"""
+
+goose = r"""
+The goose habitat...
+                                          _
+                                      ,-"" "".
+                                    ,'  ____  `.
+                                  ,'  ,'    `.  `._
+         (`.         _..--.._   ,'  ,'        \    \
+        (`-.\    .-""        ""'   /          (  d _b
+       (`._  `-"" ,._             (            `-(   \
+       <_  `     (  <`<            \              `-._\
+        <`-       (__< <            :
+         (__        (_<_<           ;
+    ------------------------------------------------------
+Beautiful!"""
+
+animals = [camel, lion, deer, goose, bat, rabbit]
+
+while True:
+    print("Please enter the number of the habitat you would like to view:")
