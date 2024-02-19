459114eaa6c377eba0236a5ada3f691254876e5f
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-02-19 19:35:49 +0300

    4-st stage edit

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,18 +1,28 @@
 """writing a program that implements a simple arithmetic test."""
 
-
 import random
+from typing import Tuple
 
-def generate_question(level):
+def generate_question(level: int) -> Tuple[str, str]:
+    """
+    Generates questions for the test depending on the level of difficulty.
+
+
+    Args:
+        level (int): Difficulty level (1 or 2).
+
+
+    Returns:
+        tuple: A tuple containing the question and the correct answer.
+
+    """
+    question = ""
+    answer = ""
     if level == 1:
-        # Генеруємо два випадкових числа в діапазоні від 2 до 9
         num1 = random.randint(2, 9)
         num2 = random.randint(2, 9)
-        # Вибираємо випадкову арифметичну операцію
         operator = random.choice(['+', '-', '*'])
-        # Формуємо рядок із завданням
         question = f"{num1} {operator} {num2}"
-        # Обчислюємо правильну відповідь
         if operator == '+':
             answer = num1 + num2
         elif operator == '-':
@@ -20,17 +30,23 @@ def generate_question(level):
         else:
             answer = num1 * num2
     elif level == 2:
-        # Генеруємо випадкове число від 11 до 29
         num = random.randint(11, 29)
-        # Формуємо рядок із завданням
         question = f"Square of {num}"
-        # Обчислюємо правильну відповідь
         answer = num ** 2
-    return question, answer
+    return question, str(answer)
 
-def main():
+
+def main() -> None:
+    """
+    The main function of the program controls the execution of the test and the saving of the results.
+
+    """
     while True:
-        level = input("Which level do you want? Enter a number:\n1 - simple operations with numbers 2-9\n2 - integral squares of 11-29\n> ")
+        level = input(
+            "Which level do you want? Enter a number:\n"
+            "1 - simple operations with numbers 2-9\n"
+            "2 - integral squares of 11-29\n> "
+        )
         try:
             level = int(level)
             if level not in [1, 2]:
@@ -41,20 +57,17 @@ def main():
 
         correct_answers = 0
         for _ in range(5):
-            # Генеруємо питання
             question, correct_answer = generate_question(level)
             print(question)
             while True:
                 user_answer = input("> ")
-                # Перевіряємо формат відповіді
                 try:
                     user_answer = int(user_answer)
-                    break  # Вихід з циклу, якщо відповідь у правильному форматі
+                    break
                 except ValueError:
                     print("Incorrect format. Please enter a valid integer.")
 
-            # Перевіряємо відповідь
-            if user_answer == correct_answer:
+            if user_answer == int(correct_answer):
                 print("Right!")
                 correct_answers += 1
             else:
