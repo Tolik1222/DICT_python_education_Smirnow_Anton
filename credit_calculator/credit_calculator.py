66f13ad235f6de770a45a0730edb970f24b9ed8f
Tolik1222 <a.y.smirnov@student.khai.edu>
2024-03-07 11:25:58 +0300

    4-st stage edited

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -5,43 +5,25 @@ import argparse
 import math
 import sys
 
-
-# calculates differentiated monthly payments to repay the loan.
-
-
 def calculate_diff_payment(principal, periods, interest):
     total_payment = 0
     for m in range(1, periods + 1):
         diff_payment = principal / periods + interest * (principal - principal * (m - 1) / periods)
         total_payment += math.ceil(diff_payment)
         print(f"Month {m}: payment is {math.ceil(diff_payment)}")
-    overpayment = total_payment - principal
-    print(f"Overpayment = {overpayment}")
-
-
-# calculates the monthly annuity payment to repay the loan
-
+    print_overpayment(total_payment, principal)
 
 def calculate_annuity_payment(principal, periods, interest):
     annuity_payment = principal * (interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1)
     print(f"Your annuity payment = {math.ceil(annuity_payment)}!")
-    overpayment = math.ceil(annuity_payment) * periods - principal
-    print(f"Overpayment = {overpayment}")
-
-
-# calculation of the principal amount of the loan based on the monthly payment,
-# the number of months and the interest rate.
-
+    total_payment = math.ceil(annuity_payment) * periods
+    print_overpayment(total_payment, principal)
 
 def calculate_principal(payment, periods, interest):
     principal = payment / ((interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
     print(f"Your loan principal = {math.floor(principal)}!")
-    overpayment = payment * periods - math.floor(principal)
-    print(f"Overpayment = {overpayment}")
-
-
-# calculates the number of months required to repay the loan
-
+    total_payment = payment * periods
+    print_overpayment(total_payment, math.floor(principal))
 
 def calculate_periods(principal, payment, interest):
     periods = math.log(payment / (payment - interest * principal), 1 + interest)
@@ -54,36 +36,44 @@ def calculate_periods(principal, payment, interest):
         print(f"It will take {years} years to repay this loan!")
     else:
         print(f"It will take {years} years and {months} months to repay this loan!")
+    total_payment = payment * periods
+    print_overpayment(total_payment, principal)
 
-
-# takes a list of command line arguments and parses it to obtain parameter values
-# such as payment type, loan principal amount, monthly payment, number of periods and annual interest rate.
-
+def print_overpayment(total_payment, principal):
+    overpayment = total_payment - principal
+    print(f"Overpayment = {overpayment // 1}")
 
 def parse_arguments(args):
     parser = argparse.ArgumentParser(description="Credit Calculator")
-    parser.add_argument("--type", choices=["diff", "annuity"],
-                        help="type of payment: 'diff' for differentiated and 'annuity' for annuity")
+    parser.add_argument("--type", choices=["diff", "annuity"], help="type of payment: 'diff' for differentiated and 'annuity' for annuity")
     parser.add_argument("--principal", type=float, help="loan principal amount")
     parser.add_argument("--payment", type=float, help="monthly payment amount")
     parser.add_argument("--periods", type=int, help="number of periods to repay the loan")
     parser.add_argument("--interest", type=float, help="annual interest rate")
     return parser.parse_args(args)
 
-
-# checks entered command line arguments for correctness
-
-
 def validate_arguments(args):
-    if args.type is None or (args.type == "diff" and args.payment is not None) or \
-            (args.type == "annuity" and (args.principal is None or args.periods is None or args.interest is None)):
+    if args.type is None or args.interest is None:
         print("Incorrect parameters")
         sys.exit(1)
-
-
-# is called when the program starts and performs all the necessary actions to calculate the credit
-# based on the entered command line arguments.
-
+    if args.type == "diff" and args.payment is not None:
+        print("Incorrect parameters")
+        sys.exit(1)
+    if args.type == "annuity" and args.principal is None and args.payment is None and args.periods is None:
+        print("Incorrect parameters")
+        sys.exit(1)
+    if args.principal is not None and args.principal < 0:
+        print("Incorrect parameters")
+        sys.exit(1)
+    if args.periods is not None and args.periods < 0:
+        print("Incorrect parameters")
+        sys.exit(1)
+    if args.interest is not None and args.interest < 0:
+        print("Incorrect parameters")
+        sys.exit(1)
+    if args.payment is not None and args.payment < 0:
+        print("Incorrect parameters")
+        sys.exit(1)
 
 def main():
     args = parse_arguments(sys.argv[1:])
@@ -98,7 +88,6 @@ def main():
             calculate_principal(args.payment, args.periods, args.interest / 100 / 12)
         elif args.periods is None:
             calculate_periods(args.principal, args.payment, args.interest / 100 / 12)
-
 
 if __name__ == "__main__":
     main()
